27bab5c534dee6abc38ee4d0338b9d4983d80fc7
sadeghh <research@rayana.ir>
2014-03-02 18:55:24 +0330

    add defualt page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DemoApp::Application.routes.draw do
+ # match 'home', :to => "static#index"
+ # root :to => "static#index"
   resources :microposts
 
   resources :users
@@ -7,7 +9,7 @@ DemoApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
